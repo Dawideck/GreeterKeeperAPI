333eb6b9c36564478316f2f60c5009db140b5fcb
Dawideck <dawid.tenderenda@gmail.com>
2024-08-06 23:15:24 +0200

    pass

--- validators.py
+++ validators.py
@@ -7,15 +7,15 @@ forbidden_ids = [420, 2137, 69]
 def is_id_correct(id) -> bool:
         ic(id)
         if id in teacher_ids or id in forbidden_ids:
-                return False
+            return False
         return True
 
 def is_student_registered(student_id: int, students_list: AllAttendees) -> bool:
         ic(students_list["attendees"])
         ic(student_id)
         for student in students_list["attendees"]:
-                if student["id"] == student_id:
-                        return True
+            if student["id"] == student_id:
+                return True
         return False
 
 def is_student_already_logged(student_id: int, date:str, students_list) -> bool:
